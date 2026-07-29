3e1d1cd505d11f04a7a0bc2b79409092aa874539
DerpPrincess <nekokagamines@gmail.com>
2026-07-29 08:41:02 -0500

    wip

--- mods/blackout/source/Blackout.cs
+++ mods/blackout/source/Blackout.cs
@@ -4,6 +4,8 @@ using RecompOne.Runtime.Context;
 using RecompOne.Runtime.Memory;
 using RecompOne.Runtime.Hardware;
 using Sotn;
+using Recompiled;
+using RecompOne.Runtime.Events;
 
 /*
 [PreHook("dra", "func_800FE044")]
@@ -13,13 +15,43 @@ using Sotn;
 
 public class BlackoutMod : IMod
 {
+    public bool giveMoreStartingMP = false;
+
     public void OnLoad()
     {
-        /* Gives Soul of Bat, Echo of Bat, Spirit Orb, and Faerie Scroll */
+        Event.AddListener<PlayerLoadedEvent>(Init);
+        Event.AddListener<VSyncEvent>(OnVSyncEvent);
+    }
+
+    public void OnVSyncEvent(VSyncEvent vse)
+    {
+        
+    }
+
+    public void Init(PlayerLoadedEvent ple)
+    {
+        GiveBlackoutRelics();
+        if (giveMoreStartingMP) GivePlayerHigherStartingMP();
+        
+    }
+
+    /* Gives Soul of Bat, Echo of Bat, Spirit Orb, and Faerie Scroll */
+    private void GiveBlackoutRelics()
+    {
         Inventory.GiveRelic(Relic.SoulOfBat, true);
         Inventory.GiveRelic(Relic.EchoOfBat, true);
         Inventory.GiveRelic(Relic.SpiritOrb, true);
         Inventory.GiveRelic(Relic.FaerieScroll, true);
+    }
+
+    private void GivePlayerHigherStartingMP()
+    {
+        Player.MpMax = 200;
+    }
+
+    private uint GetEchoOfBatTimer()
+    {
+        return 0;
     }
 
     [Replace("OVERLAY", "function")]
